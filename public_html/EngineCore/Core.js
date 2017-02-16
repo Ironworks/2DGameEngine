92a5f2b8f25d3bc0f9f0389e6654c5a211e3b750
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-02-16 19:04:16 +0000

    Created Vec2 class 	new file:   .bowerrc 	new file:   Gruntfile.js 	new file:   bower.json 	new file:   gulpfile.js 	new file:   nbproject/private/private.properties 	new file:   nbproject/private/private.xml 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   package.json 	new file:   public_html/EngineCore/Core.js 	modified:   public_html/EngineCore/UserControl.js 	new file:   public_html/Lib/Vec2.js 	new file:   public_html/index.html

---
+++ public_html/EngineCore/Core.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var gEngine = gEngine || {};
+gEngine.Core = (function () {
+    var mCanvas, mContext, mWidth = 800, mHeight = 450;
+    mCanvas = document.getElementById('canvas');
+    mContext = mCanvas.getContext('2d');
+    mCanvas.height = mHeight;
+    mCanvas.width = mWidth;
+    
+    var mPublic = {
+        mWidth: mWidth,
+        mHeight: mHeight,
+        mContext: mContext
+    };
+    
+    return mPublic;
+}());
+
--- public_html/EngineCore/UserControl.js
+++ public_html/EngineCore/UserControl.js
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//Added a comment 
 function userControl(event) {
     var keycode;
     if (window.event) { //IE
